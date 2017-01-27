e84a642e27a563cb3b30b21dd169ab65d896d6a7
Anuj Gupta <anupta45@gmail.com>
2017-01-27 16:49:48 +0530

    Revert "Revert "Gallery- celebrations - images linked""
    This reverts commit 0dd9993c918e953089739fa81e08fa52f75fbcfe.

---
+++ Apache/kashish/alumni.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<!-- start: Meta -->
+	<meta charset="utf-8">
+	<title>Contact Us</title><link rel="icon" type="image/png" href="http://visionmanit.in/img/favicon.png"> 
+	<meta name="description" content="GotYa Free Bootstrap Theme"/>
+	<meta name="keywords" content="Template, Theme, web, html5, css3, Bootstrap" />
+	<meta name="author" content="?ukasz Holeczek from creativeLabs"/>
+	<!-- end: Meta -->
+	
+	<!-- start: Mobile Specific -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<!-- end: Mobile Specific -->
+	
+	<!-- start: Facebook Open Graph -->
+	<meta property="og:title" content=""/>
+	<meta property="og:description" content=""/>
+	<meta property="og:type" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:image" content=""/>
+	<!-- end: Facebook Open Graph -->
+
+    <!-- start: CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/bootstrap-responsive.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Droid+Sans:400,700">
+	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Droid+Serif">
+	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Boogaloo">
+	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Economica:700,400italic">
+	<!-- end: CSS -->
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+	
+	<!--start: Header -->
+	<header style="height:70px">
+		
+		<!--start: Container -->
+		<div class="container">
+			
+			<!--start: Row -->
+			<div class="row">
+					
+				<!--start: Logo -->
+				<div class="logo span3">
+						
+<a class="brand" href="Home.php"><img src="img/logo.png" style="width: 170px;height: 70px;margin-top: 3px;"></a>
+						
+				</div>
+				<!--end: Logo -->
+					
+				<!--start: Navigation -->
+				<div class="span9">
+						
+					<div class="navbar navbar-inverse">
+			    		<div class="navbar-inner">
+			        		
+			          			<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+			            			<span class="icon-bar"></span>
+			            			<span class="icon-bar"></span>
+			            			<span class="icon-bar"></span>
+			          			</a>
+			          			<div class="nav-collapse collapse">
+			            			<ul class="nav">
+			              				<li><a href="Home.php">Home</a></li>
+			              				<li><a href="About.php">About</a></li>
+										<li><a href="Gallery.php">Gallery</a></li>
+										<li><a href="Events.php">Events</a></li>
+			              				<li class="active"><a href="Contact.php">Contact Us</a></li>
+			              				<li class="dropdown">
+			                				<a href="#" class="dropdown-toggle" data-toggle="dropdown"><b class="caret"></b></a>
+			                				<ul class="dropdown-menu">
+			                  					<!--<li><a href="#">Student ambassador</a></li>
+			                  					<li><a href="#">Alumni cell</a></li>
+			                  					<li><a href="#">Something else here</a></li>
+			                  					<li class="divider"></li>
+			                  					<li class="nav-header">Nav header</li>
+			                  					<li><a href="#">Separated link</a></li>
+			                  					<li><a href="#">One more separated link</a></li>-->
+			                				</ul>
+			              				</li> 
+			            			</ul>
+			          			</div>
+			        		
+			      		</div>
+			    	</div>
+					
+				</div>	
+				<!--end: Navigation -->
+					
+			</div>
+			<!--end: Row -->
+			
+		</div>
+		<!--end: Container-->			
+			
+	</header>
+	<!--end: Header-->
+    <!-- start: Footer Menu -->
+	<div class="footer-bottom" style="text-align:center;padding-bottom: 3rem;padding-top: 24px;padding-bottom: 10px;font-size: 16px ;font-family: raleway-medium,sans-serif;background: #14181E;">
+    <div class="row" >
+
+        <div > <span>&copy; Copyright Vision.</span><br><span>Designed and Maintained by Vision CSE</span></div>
+    </div>
+    <!-- end: Footer Menu -->
+</body>
+</html>
